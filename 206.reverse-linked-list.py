b1f70a83c4290119c37721ad2fbc335e9976efe6
James Tagaca <jtagaca@csub.edu>
2021-08-26 19:44:39 -0700

    save

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -1,3 +1,8 @@
+# @before-stub-for-debug-begin
+from python3problem206 import *
+from typing import *
+# @before-stub-for-debug-end
+
 #
 # @lc app=leetcode id=206 lang=python3
 #
@@ -10,7 +15,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# 1 → 2 → 3 → Ø
+
+
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dasdasd
+    def reverseList(self, head):
+        currentNode = head
+        prev = None
+        while currentNode:
+            nxt = currentNode.next
+            # severing the link and using this to chain
+            # 1-> None
+            # 2 -> 1 → None etc
+            currentNode.next = prev
+            # updating the value to our current node
+            prev = currentNode
+            # updating to the saved value
+            currentNode = nxt
+        return prev
+
+
 # @lc code=end
