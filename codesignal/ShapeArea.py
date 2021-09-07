92e7570a23974dc450a9d373c2fa0818f924c46d
James Tagaca <jtagaca@csub.edu>
2021-09-06 20:18:49 -0700

    save

---
+++ codesignal/ShapeArea.py
@@ -0,0 +1,6 @@
+def shapeArea(n):
+    #photo "C:\Users\Admin\Documents\Screenshot 2021-09-06 201650.jpg"
+    # this works because for "C:\Users\Admin\Documents\Screenshot 2021-09-06 201754.jpg"
+    # for every red we are having n*n and for the blue we have n-1 to the power of 2  
+    return n**2 + (n-1)**2
+
