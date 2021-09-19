3a5fbe982ed3d0ae5651aad8276491bc33fa1d89
James Tagaca <jtagaca@csub.edu>
2021-09-18 17:30:37 -0700

    save

---
+++ 33.search-in-rotated-sorted-array.py
@@ -0,0 +1,53 @@
+# @before-stub-for-debug-begin
+from python3problem33 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # min l # max r 
+        #check which is the sorted partition
+        #binary search 
+        #target is in range of the current partition
+        l,r= 0, len(nums)-1
+        #[4,5,6,7,0,1,2]
+        #0
+        #0+6
+        #6//2 =3 
+        #middle=7
+        # this works because we are either selecting 
+        # the correct partitioned array for each time 
+        # and so we then check if the value is within our range 
+        # if it does then it we will just update our middle
+        while l<=r:
+            middle=(l+r)//2
+            
+            currentMiddle=nums[middle]
+            if currentMiddle==target:
+                return middle
+               #check which is the sorted partition
+            if nums[l]<currentMiddle:
+                #left sorted partition
+                if target>=nums[l] and target<currentMiddle:
+                    r= middle-1
+                else:
+                    l= middle+1
+                    
+            else:
+                if target==nums[r]:
+                    l=middle+1
+                if target<=nums[r] and target>currentMiddle:
+                    l=middle+1
+                else:
+                    r=middle-1
+        return -1
+        
+# @lc code=end
+
